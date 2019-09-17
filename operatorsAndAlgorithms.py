e31987bff63c6612169900400f719fab63681536
Ashish Garg <mragarg@gmail.com>
2019-09-17 17:48:37 -0400

    Created first individual and return relevant information

--- operatorsAndAlgorithms.py
+++ operatorsAndAlgorithms.py
@@ -12,3 +12,9 @@ creator.create("Individual", list, fitness=creator.FitnessMin)
 toolbox = base.Toolbox()
 toolbox.register("attr_float", random.random)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=IND_SIZE)
+
+# building the first individual
+ind1 = toolbox.individual()
+
+print(ind1)
+print(ind1.fitness.valid)
