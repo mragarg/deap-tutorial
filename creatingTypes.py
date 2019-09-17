6ed8a687fb66494659d7e1a675c36cb410934af8
Ashish Garg <mragarg@gmail.com>
2019-09-17 17:37:57 -0400

    Creating Types of Permutations, Type Floating Point is commented out

--- creatingTypes.py
+++ creatingTypes.py
@@ -4,11 +4,22 @@ from deap import base
 from deap import creator
 from deap import tools
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+# List of Floats
+# creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+# creator.create("Individual", list, fitness=creator.FitnessMax)
+
+# IND_SIZE = 10
+
+# toolbox = base.Toolbox()
+# toolbox.register("attr_float", random.random) # register (alias, function)
+# toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=IND_SIZE)
+
+# Permutation
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
 IND_SIZE = 10
 
 toolbox = base.Toolbox()
-toolbox.register("attr_float", random.random)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=IND_SIZE)
+toolbox.register("indices", random.sample, range(IND_SIZE), IND_SIZE)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
