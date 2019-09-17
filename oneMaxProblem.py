34f78a755bbe23356a64e8593277c5c9ef799676
Ashish Garg <mragarg@gmail.com>
2019-09-17 17:53:32 -0400

    Constructing the oneMaxProblem

---
+++ oneMaxProblem.py
@@ -0,0 +1,12 @@
+import random
+from deap import creator, base, tools, algorithms
+
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+toolbox.register("attr_bool", random.randint, 0, 1)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=100)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
